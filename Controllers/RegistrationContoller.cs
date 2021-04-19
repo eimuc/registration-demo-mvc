d25cd0688433ee87892e87fed879a1e994df4878
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-04-19 15:32:36 +0300

    adde registration model, controller, view

---
+++ Controllers/RegistrationContoller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationDemoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationDemoMVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post(RegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationDemoMVC.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]*$")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]*$")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Range(18, 100)]
+        public int Age { get; set; }
+
+    }
+}
